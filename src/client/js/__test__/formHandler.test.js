6a6b31a0424e68193db02a130cb174709e638d99
Esraa Afifi <esraa.afifi@vida.com>
2020-09-10 23:08:50 -0700

    make it better

--- src/client/js/__test__/formHandler.test.js
+++ src/client/js/__test__/formHandler.test.js
@@ -1,11 +1,43 @@
 import { handleSubmit } from "../formHandler";
+import { checkValidUrl } from "../urlChecker";
 
 describe("Testing the handle submit functionality", () => {
   test("Testing the handleSubmit() function", () => {
-    // Define the input for the function, if any, in the form of variables/array
-    // Define the expected output, if any, in the form of variables/array
-    // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
-    // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
+    const event = new Event('click', { "target": { "value": 8 } })
+    jest.spyOn(event, 'preventDefault');
+    document.body.innerHTML += `
+    <section>
+    <form class="" onsubmit="return Client.handleSubmit(event)">
+        <label class="title" for="name">URL:
+        </label>
+        <input id="name" type="text" name="input" value="" placeholder="Name">
+        <input id="submit" type="submit" name="" value="Submit" onclick="return Client.handleSubmit(event)" onsubmit="return Client.handleSubmit(event)">
+        <p id="error" class="error" hidden>Please input a valid URL</p>
+    </form>
+
+        <section class="results">
+            <strong class="title">NLP Results:</strong>
+            <ul type="circle">
+                <li>
+                    <div id="subjectivity"></div>
+                </li>
+                <li>
+                    <div id="score"></div>
+                </li>
+                <li>
+                    <div id="agreement"></div>
+                </li>
+                <li>
+                    <div id="confidence"></div>
+                </li>
+                <li>
+                    <div id="irony"></div>
+                </li>
+            </ul>
+        </section>
+  </section>`;
+    handleSubmit(event)
     expect(handleSubmit).toBeDefined();
+    expect(event.preventDefault).toHaveBeenCalled()
   });
 });
--- src/client/js/__test__/urlChecker.test.js
+++ src/client/js/__test__/urlChecker.test.js
@@ -1,11 +1,14 @@
 import { checkValidUrl } from "../urlChecker";
 
 describe("Testing the check url validation functionality", () => {
-  test("Testing the checkValidUrl() function", () => {
-    // Define the input for the function, if any, in the form of variables/array
-    // Define the expected output, if any, in the form of variables/array
-    // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
-    // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
+  test("Testing the checkValidUrl() returns false for invalid url", () => {
+    const url = ''
     expect(checkValidUrl).toBeDefined();
+    expect(checkValidUrl(url)).toEqual(false)
+  });
+  test("Testing that checkValidUrl() return true with valid url", () => {
+    const url = 'https://valid.com'
+    expect(checkValidUrl).toBeDefined();
+    expect(checkValidUrl(url)).toEqual(true)
   });
 });
